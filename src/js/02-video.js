1e36f9c446156d5da6541d9f486bc0c3cbeab48c
Stanislav Zabiyaka <stzabiyaka@gmail.com>
2022-05-20 18:49:47 +0300

    video and feedback  scripts creation
    vimeo player js created,
    feedback form js created,
    throttle function imported

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,40 @@
+import Player from '@vimeo/player';
 
+const throttle = require('lodash.throttle');
+
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+playerInitialisation();
+
+player.on('timeupdate', throttle(updateLocalStorage, 1000));
+
+function updateLocalStorage () {
+    player.getCurrentTime().then(function(seconds) {    
+        localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+    }).catch(function(error) {
+        console.log('Error occured:', error.name);
+    });
+}
+
+function playerInitialisation () {
+    const localStorageValue = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (!localStorageValue) {
+        return;
+    }
+
+        player.setCurrentTime(localStorageValue).then(function(seconds) {}).catch(function(error) {
+            switch (error.name) {
+                case 'RangeError':
+                    console.log('The time was less than 0 or greater than the video’s duration');
+                    break;
+        
+                default:
+                    console.log('Error occured:', error.name);
+                    break;
+            }
+        });
+    }
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,59 @@
+const throttle = require('lodash.throttle');
+
+const formRef = document.querySelector('.feedback-form');
+
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+const formState = {};
+
+formInitialisation();
+
+
+formRef.addEventListener('input', throttle(updateLocalStorage, 500));
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (evt) {
+    evt.preventDefault();
+    formStateUpdate();
+    localStorage.removeItem(LOCALSTORAGE_KEY);
+    console.log(formState);
+    formRef.reset();
+}
+
+function formStateUpdate () {
+    formState.email = formRef.elements.email.value;
+    formState.message = formRef.elements.message.value;
+}
+
+function updateLocalStorage () {
+        formStateUpdate();
+
+    try {
+        localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formState));
+      } catch (error) {
+        console.error("Set state error: ", error.message);
+      }
+}
+
+
+function formInitialisation () {
+
+    const localStorageValue = localStorage.getItem(LOCALSTORAGE_KEY);
+
+    if (!localStorageValue) {
+        return
+    }
+
+            try {
+            const parsedFormState = JSON.parse(localStorageValue);
+
+            formRef.elements.email.value = parsedFormState.email;
+            formRef.elements.message.value = parsedFormState.message;  
+          } catch (error) {
+            console.log('Get state error:', error.message);
+          }
+
+            
+    }
 
